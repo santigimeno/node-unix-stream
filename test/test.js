c8b35cd5031d6ac65070a3481985e33e14d583d1
Santiago Gimeno <santiago.gimeno@gmail.com>
2014-05-03 01:06:10 +0200

    Improve tests

--- test/test.js
+++ test/test.js
@@ -4,43 +4,75 @@ var fs = require('fs');
 var unix = require('../src/unix_stream');
 
 describe('Echo server', function() {
+    before(function(done) {
+        if (fs.existsSync('/tmp/remote_path')) {
+            fs.unlinkSync('/tmp/remote_path');
+        }
 
-    it('client bound', function(done) {
-        var socket;
-        var server = net.createServer(function(con) {
-            assert.equal(con.path(), socket.remotePath);
-            assert.equal(con.remotePath, socket.path());
+        if (fs.existsSync('/tmp/local_path')) {
+            fs.unlinkSync('/tmp/local_path');
+        }
+
+        this.server = net.createServer();
+        this.server.listen('/tmp/remote_path', done);
+        this.server.on('error', done);
+    });
+
+    it('should emit error if binding to an existing path', function(done) {
+        fs.openSync('/tmp/local_ppp', 'w');
+        var socket = unix.createSocket('/tmp/local_ppp');
+        socket.on('error', function(err) {
+            assert.equal(err.syscall, 'bind');
             done();
-        });
-
-        /* Unlink /tmp/tmp_server if exists, then listen */
-        if (fs.existsSync('/tmp/remote_path')) fs.unlinkSync('/tmp/remote_path');
-        server.listen('/tmp/remote_path', function() {
-            /* Create Unix socket bound to /tmp/local_path */
-            if (fs.existsSync('/tmp/local_path')) fs.unlinkSync('/tmp/local_path');
-            socket = unix.createSocket('/tmp/local_path');
-            /* Connect to /tmp/remote_path */
-            socket.connect({ path : '/tmp/remote_path' });
         });
     });
 
-    it('client not bound', function(done) {
-        var socket;
-        var server = net.createServer(function(con) {
-            assert.equal(con.path(), socket.remotePath);
+    it('should set the local and remote path correctly', function(done) {
+        var self = this;
+        this.server.once('connection', function(con) {
+            assert.equal(con.path(), self.socket.remotePath);
+            assert.equal(con.localPath, self.socket.remotePath);
+            assert.equal(con.remotePath, self.socket.path());
+            done();
+        })
+        
+        this.socket = unix.createSocket('/tmp/local_path');
+        this.socket.connect({ path : '/tmp/remote_path' });
+    });
+
+    it('should set the local and remote path correctly', function(done) {
+        var self = this;
+        this.server.once('connection', function(con) {
+            assert.equal(con.path(), self.socket.remotePath);
+            assert.equal(con.localPath, self.socket.remotePath);
             assert.equal(con.remotePath, null);
-            assert.equal(socket.path(), null);
+            assert.equal(self.socket.path(), null);
+            assert.equal(self.socket.localPath, null);
             done();
         });
 
-        /* Unlink /tmp/tmp_server if exists, then listen */
-        if (fs.existsSync('/tmp/remote_path')) fs.unlinkSync('/tmp/remote_path');
-        server.listen('/tmp/remote_path', function() {
-            /* Create Unix socket bound to /tmp/local_path */
-            if (fs.existsSync('/tmp/local_path')) fs.unlinkSync('/tmp/local_path');
-            socket = unix.createSocket();
-            /* Connect to /tmp/remote_path */
-            socket.connect({ path : '/tmp/remote_path' });
-        });
+        this.socket = unix.createSocket();
+        this.socket.connect({ path : '/tmp/remote_path' });
+    });
+
+    afterEach(function(done) {
+        if (fs.existsSync('/tmp/local_path')) {
+            fs.unlinkSync('/tmp/local_path');
+        }
+
+        if (this.socket) {
+            this.socket.on('end', done);
+            this.socket.end();
+        } else {
+            done();
+        }
+    });
+
+    after(function(done) {
+        this.server.close(done);
+        if (fs.existsSync('/tmp/remote_path')) {
+            fs.unlinkSync('/tmp/remote_path');
+        }
     });
 });
+
